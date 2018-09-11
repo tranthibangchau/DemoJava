a30620d3e6553b34509c88d6772ca6235ae2d1a5
tran.thi.bang.chau <tran.thi.bang.chau@framgia.com>
2018-09-11 18:43:35 +0700

    commit demo java

---
+++ src/hinhChunhat.java
@@ -0,0 +1,18 @@
+
+public class hinhChunhat {
+	protected int chieuDai, chieuRong;
+	 
+    public hinhChunhat(int chieuDai, int chieuRong) {
+        super();
+        this.chieuDai = chieuDai;
+        this.chieuRong = chieuRong;
+    }
+ 
+    public int tinhChuVi() {
+        return (chieuDai + chieuRong) * 2;
+    }
+     
+    public int tinhDienTich() {
+        return chieuDai * chieuRong;
+    }
+}
---
+++ src/hinhVuong.java
@@ -0,0 +1,6 @@
+
+public class hinhVuong extends hinhChunhat {
+     public hinhVuong(int a) {
+        chieuDai = chieuRong = a;
+    }
+}
