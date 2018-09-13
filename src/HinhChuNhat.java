f9483cc0e2c958bbdb8426fad1e0a5955a3d187a
do.thi.minh.hoa <do.thi.minh.hoa@framgia.com>
2018-09-13 16:35:28 +0700

    Hoa update

---
+++ src/HinhChuNhat.java
@@ -0,0 +1,8 @@
+
+public class HinhChuNhat extends Hinh{
+	
+public HinhChuNhat(float canh1, float canh2) {
+		super(canh1, canh2);
+		// TODO Auto-generated constructor stub
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,26 @@
+
 public class Main {	
+	public static void main(String[] args) {
+
+	
+		System.out.println("\n\n________________Hinh Chu Nhat__________________");
+		HinhChuNhat hcn = new HinhChuNhat(4,5);
+		hcn.htThongTin("Chu Nhat");
+		
+		System.out.println("\n\n________________Hinh Chu Vuong__________________");
+		HinhVuong hv = new HinhVuong(3, 3);
+		hv.htThongTin("Vuong");
+		
+		
+		System.out.println("\n\n________________Hinh Tam Giac__________________");
+		HinhTamGiac htg = new HinhTamGiac();
+		
+		
+		System.out.println("Chu vi tam giac: "+htg.Chuvi(1, 2, 3));
+		System.out.println("Dien tich tam giac: "+htg.Dientich(1, 2, 3));
+
+		
+		
+	}
  
 }
